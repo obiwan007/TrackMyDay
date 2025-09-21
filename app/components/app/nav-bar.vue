f1b3f30d11839c68027df9eb1ea901353368ddba
obiwan007 <markus.miertschink@hotmail.com>
2025-09-21 09:59:48 +0200

    First commit

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+	<nav class="container">
+		<NuxtLink :to="{ name: 'index' }">
+			<ul>
+				<li><strong>My Weight Tracker</strong></li>
+			</ul>
+		</NuxtLink>
+		<ul>
+			<li><a href="#">About</a></li>
+			<NuxtLink :to="{ name: 'create' }">Create</NuxtLink>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Products</a></li>
+		</ul>
+	</nav>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	modules: ['@nuxt/eslint'],
+	devtools: { enabled: true },
+	css: ['@picocss/pico'],
+	compatibilityDate: '2025-07-15',
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: 'double',
+				indent: 'tab',
+			},
+		},
+	},
+});
